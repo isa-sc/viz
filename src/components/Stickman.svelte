9aa0a1413b62e25e1453d6c8cde14c71584dc812
Isabel Scholz <isabel.scholz@student.kuleuven.be>
2024-05-02 18:09:28 +0200

    first commit

---
+++ src/components/Stickman.svelte
@@ -0,0 +1,58 @@
+<body> 
+    <svg width= {widthx} height = {heighty}>
+        <circle cx="{startx}" cy="{headcenter}" r="{headsize}"/>
+        <path d="M {startx},{starty} l -10,10 l 10, -10 l 10, 10"/>
+        <path d="M {startx},{starty} l 0,-{stickheight}" />
+        <path d="M {startx}, {armheight} l -15, 10 l 15, -10 l 15, 10"/>
+        <image class="crown" href="crown.png" x= {crownstartx} y = {crownstarty} 
+            width ={crownsize} height={crownsize}/>
+        <image class= "backpack" href="backpack.png" x="{backpackx}" y="{backpacky}" 
+            width="{backpacksize}" height={backpacksize}/>
+        <image class= "potion" href="potion.png" x={potionx} y={potiony} 
+            width={potionsize} height={potionsize}/>
+        
+    </svg> 
+    <p>My name is {name}</p>
+    <p>Number of orders:{scaledOrders}</p>
+    <p>Money earned:{scaledMoney}</p>
+    <p>Nr of adventure products: {scaledAdv}</p>
+    <p>Nr of Commission products: {scaledCom}</p>
+    <p>Nr of Luxury products: {scaledLux}</p>
+</body>
+<script>
+    export let scaledOrders;
+    export let scaledMoney;
+    export let scaledLux;
+    export let scaledAdv;
+    export let scaledCom;
+    export let name;
+    let widthx = 100;
+    let heighty = 190;
+    let stickheight = scaledOrders *100;
+    let headsize = scaledMoney *20
+    let crownsize = scaledLux *40;
+    let backpacksize = scaledAdv *40;
+    let potionsize = scaledCom *40;
+    let startx = widthx/2
+    let starty = heighty-10
+    let armheight = starty - (stickheight*2)/3
+    let headcenter = starty - stickheight - headsize 
+    let crownstarty = headcenter - headsize -crownsize*0.85
+    let crownstartx = startx - (crownsize*0.5)
+    let backpackx = startx-10- backpacksize
+    let backpacky = armheight +10
+    let potionx = startx+ 10- (potionsize/8)
+    let potiony = armheight +10
+    
+
+</script>
+
+<style> 
+    circle , path{
+        fill: none;
+        stroke: black;
+    }
+    p{
+        opacity: 0.3;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    import Stickman from '../components/Stickman.svelte';   
+    let data = [
+        {name: 'Balthazaar Viscari', orders:0.2, money: 0.0733, adv: 0.5, comission: 0.7, luxury: 0.3},
+        {name: 'Bubbles',           orders:0.78, money: 0.33, adv: 0.6, comission: 0.3, luxury: 0.5},
+        {name: 'Balthazaar Viscari', orders:0.48, money: 0.733, adv: 1, comission: 1, luxury: 0.1},
+        {name: 'Balthazaar Viscari', orders:0.58, money: 0.43, adv: 1, comission: 1, luxury: 0.7},
+        {name: 'Balthazaar Viscari', orders:0.68, money: 0.83, adv: 1, comission: 1, luxury: 0.3},
+        {name: 'Balthazaar Viscari', orders:1, money: 1, adv: 0.1, comission: 0.2, luxury: 1},
+        {name: 'Balthazaar Viscari', orders:0.28, money: 0.23, adv: 0.7, comission: 0.9, luxury: 0},
+        {name: 'Balthazaar Viscari', orders:0.88, money: 0.13, adv: 0.5, comission: 0.1, luxury: 1}
+    ];
+ </script>
+
+
+<style>
+    .stickmen-container {
+        display: flex;
+    }
+    .stickman{
+        opacity: 0.5;
+    }
+    .stickman:hover {
+        opacity: 1; /* increase opacity when hovered over */
+    }
+</style>
+
+<div class="stickmen-container">
+    {#each data as dp}
+        <div class="stickman">
+            <Stickman name = {dp.name} scaledMoney= {dp.money} scaledOrders = {dp.orders} scaledAdv = {dp.adv} scaledCom = {dp.comission} scaledLux= {dp.luxury}/>
+        </div>
+    {/each}
+</div>
+
