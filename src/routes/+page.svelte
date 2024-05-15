03cca2670e29509dc02816cfea2e355e7f2b852e
Isabel Scholz <isabel.scholz@student.kuleuven.be>
2024-05-15 08:49:33 +0200

    final changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,106 @@
-<script>
-    import Stickman from '../components/Stickman.svelte';   
+<script lang="ts">
+    import Stickman from '../components/Stickman.svelte';
+
     let data = [
-        {name: 'Balthazaar Viscari', orders:0.2, money: 0.0733, adv: 0.5, comission: 0.7, luxury: 0.3},
-        {name: 'Bubbles',           orders:0.78, money: 0.33, adv: 0.6, comission: 0.3, luxury: 0.5},
-        {name: 'Balthazaar Viscari', orders:0.48, money: 0.733, adv: 1, comission: 1, luxury: 0.1},
-        {name: 'Balthazaar Viscari', orders:0.58, money: 0.43, adv: 1, comission: 1, luxury: 0.7},
-        {name: 'Balthazaar Viscari', orders:0.68, money: 0.83, adv: 1, comission: 1, luxury: 0.3},
-        {name: 'Balthazaar Viscari', orders:1, money: 1, adv: 0.1, comission: 0.2, luxury: 1},
-        {name: 'Balthazaar Viscari', orders:0.28, money: 0.23, adv: 0.7, comission: 0.9, luxury: 0},
-        {name: 'Balthazaar Viscari', orders:0.88, money: 0.13, adv: 0.5, comission: 0.1, luxury: 1}
+        { firstname: "T'avin", name: " T'soban", orders: 1, money: 1, adv: 1, comission: 1, luxury: 1 },
+        { firstname: '', name: 'Bubbles', orders: 0.723, money: 0.745, adv: 0.813, comission: 0.422, luxury: 0.778 },
+        { firstname: 'Kay', name: 'Glimshine', orders: 0.261, money: 0.263, adv: 0.494, comission: 0.011, luxury: 0.028 },
+        { firstname: 'Bruno', name: 'Notlot', orders: 0.250, money: 0.239, adv: 0.280, comission: 0.06, luxury: 0.252 },
+        ];
+ 
+    let data2 = [
+        { firstname: 'Balthazaar', name: 'Viscari', orders: 0.0748, money: 0.0732, adv: 0.123, comission: 0, luxury: 0.03 },
+        { firstname: 'Lanec', name: 'Lot', orders: 0.096, money: 0.103, adv: 0.104, comission: 0.074, luxury: 0.1145 },
+        { firstname: 'Shade', name: 'Tabaxii', orders: 0.118, money: 0.125, adv: 0.182, comission: 0, luxury: 0.092 },
+        { firstname: 'Gudren', name: 'Rockseeker', orders: 0.076, money: 0.081, adv: 0.007, comission: 0.158, luxury: 0.159 }
     ];
- </script>
+</script>
+
+<div class="stickmen-container">
+    {#each data as dp}
+        <div>
+            <Stickman
+                scaledMoney={dp.money}
+                scaledOrders={dp.orders}
+                scaledAdv={dp.adv}
+                scaledCom={dp.comission}
+                scaledLux={dp.luxury}
+                firstname = {dp.firstname}
+                lastname= {dp.name}
+            />
+            <p class="stickman" >
+                Number of orders: {Math.round(dp.orders * 33790).toLocaleString()} <br>
+                Money earned: {Math.round(dp.money * 910558306.7).toLocaleString()}$ <br>
+                {#if dp.adv*465134305.8 > dp.comission*130412395.3}
+                    {#if dp.adv*465134305.8 < dp.luxury*315011605.6}
+                        most popular line: Luxury
+                    {:else}
+                        most popular line: Adventure
+                    {/if}
+                {:else if dp.comission*130412395.3 > dp.luxury*315011605.6}
+                    most popular line: Commission
+                {:else}
+                    most popular line: Luxury
+                {/if} <br>
+                Luxury: {Math.round(dp.luxury * 315011605.6).toLocaleString()}$ <br>
+                Adventure: {Math.round(dp.adv * 465134305.8).toLocaleString()}$ <br>
+                Commission: {Math.round(dp.comission * 130412395.3).toLocaleString()}$
+            </p>
+        </div>
+                
+    {/each}
+</div>
+
+<div class="stickmen-container">
+    {#each data2 as dp}
+        <div>
+            <Stickman
+                scaledMoney={dp.money}
+                scaledOrders={dp.orders}
+                scaledAdv={dp.adv}
+                scaledCom={dp.comission}
+                scaledLux={dp.luxury}
+                firstname = {dp.firstname}
+                lastname= {dp.name}
+            />
+            <p class="stickman" >
+                Number of orders: {Math.round(dp.orders * 33790).toLocaleString()} <br>
+                Money earned: {Math.round(dp.money * 910558306.7).toLocaleString()}$ <br>
+                {#if dp.adv*465134305.8 > dp.comission*130412395.3}
+                    {#if dp.adv*465134305.8 < dp.luxury*315011605.6}
+                        most popular line: Luxury
+                    {:else}
+                        most popular line: Adventure
+                    {/if}
+                {:else if dp.comission*130412395.3 > dp.luxury*315011605.6}
+                    most popular line: Commission
+                {:else}
+                    most popular line: Luxury
+                {/if} <br>
+                Luxury: {Math.round(dp.luxury * 315011605.6).toLocaleString()}$ <br>
+                Adventure: {Math.round(dp.adv * 465134305.8).toLocaleString()}$ <br>
+                Commission: {Math.round(dp.comission * 130412395.3).toLocaleString()}$
+            </p>
+        </div>
+    {/each}
+</div>
 
 
 <style>
     .stickmen-container {
         display: flex;
     }
-    .stickman{
-        opacity: 0.5;
+
+    .stickman {
+        opacity: 0;
     }
+
     .stickman:hover {
         opacity: 1; /* increase opacity when hovered over */
     }
+
+    p {
+        font-family: 'Courier New', Courier, monospace;
+        font-size:x-small;
+    }
 </style>
-
-<div class="stickmen-container">
-    {#each data as dp}
-        <div class="stickman">
-            <Stickman name = {dp.name} scaledMoney= {dp.money} scaledOrders = {dp.orders} scaledAdv = {dp.adv} scaledCom = {dp.comission} scaledLux= {dp.luxury}/>
-        </div>
-    {/each}
-</div>
-
